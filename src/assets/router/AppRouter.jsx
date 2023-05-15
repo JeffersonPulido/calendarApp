6ec82a88a2e16f5363ff50125f9bb1507465e6f0
Jefferson Pulido <jeffersonpulido55@gmail.com>
2023-05-15 16:44:27 -0500

    Routes v1

---
+++ src/assets/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { CalendarPage } from '../calendar'
+
+export const AppRouter = () => {
+    
+    const authStatus = 'not-auth'
+  
+    return (
+    <>
+        <Routes>
+            {
+                (authStatus === 'not-auth')
+                ?   <Route path='/auth/*' element={<LoginPage/>}/>
+                :   <Route path='/*' element={<CalendarPage/>}/>
+            }
+
+            <Route path='/*' element={ <Navigate to="/auth/login" /> }/>
+            
+        </Routes>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { CalendarApp } from './assets/components/CalendarApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CalendarApp/>
   </React.StrictMode>,
 )
