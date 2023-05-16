f8d3a4cd174f99c764795d697579cd80cd51fb73
Jefferson Pulido <jeffersonpulido55@gmail.com>
2023-05-16 10:23:50 -0500

    Calendar event v1

--- src/assets/calendar/pages/CalendarPage.jsx
+++ src/assets/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import { Navbar } from '../components/Navbar'
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { addHours } from "date-fns";
+
+import { Navbar, CalendarBox } from "../";
+import { localizer, getMessagesES } from "../../helpers/";
+
+const events = [
+  {
+    title: "HBD",
+    notes: "Pastel y decoracion",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "BLADETIGER",
+    },
+  },
+];
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    
+    const style = {
+      backgroundColor: '#186A3B',
+      borderRadius: '0px',
+      color: 'white',
+      opacity: '0.8'
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
+
+      <Calendar
+        culture="es"
+        messages={getMessagesES()}
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+        eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarBox
+        }}
+      />
     </>
-  )
-}
+  );
+};
