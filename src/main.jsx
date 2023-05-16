e8aed871f8d68e42b3a578924bf961b4c7210660
Jefferson Pulido <jeffersonpulido55@gmail.com>
2023-05-16 09:38:32 -0500

    Navbar v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CalendarApp } from './assets/components/CalendarApp'
+import { CalendarApp } from './assets/CalendarApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
