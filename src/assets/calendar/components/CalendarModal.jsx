aa19f36b85df3c386769573de6d1f887d4271c17
Jefferson Pulido <jeffersonpulido55@gmail.com>
2023-05-18 09:01:54 -0500

    Validacion formulario v1.1 class is-invalid

--- src/assets/calendar/components/CalendarModal.jsx
+++ src/assets/calendar/components/CalendarModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { addHours, differenceInSeconds } from "date-fns";
 
 import Modal from "react-modal";
@@ -31,10 +31,8 @@ export const CalendarModal = () => {
 
   //Estado para abrir el modal
   const [isOpen, setIsOpen] = useState(true);
-  //Funcion para cerrar el modal
-  const onCloseModal = () => {
-    setIsOpen(false);
-  };
+  //Estado para controlar el submit del formulario
+  const [formSubmitted, setFormSubmitted] = useState(false)
   //Estado valores iniciales formulario
   const [formValues, setFormValues] = useState({
     title: 'Jefferson',
@@ -42,6 +40,16 @@ export const CalendarModal = () => {
     start: new Date(),
     end: addHours(new Date(), 2)
   })
+  const titleClass = useMemo(() => {
+    if ( !formSubmitted ) return '';
+    return ( formValues.title.length > 0 )
+    ? 'is-valid'
+    : 'is-invalid'
+  }, [ formValues.title , formSubmitted])
+  //Funcion para cerrar el modal
+  const onCloseModal = () => {
+    setIsOpen(false);
+  };
   //Funcion para detectar cambios con OnChange en el formulario y guardarlos en el estado
   const onInputChange = ({target}) => {
     setFormValues({
@@ -59,6 +67,7 @@ export const CalendarModal = () => {
   //Funcion para prevenir el evento predeterminado del formulario al detectar el evento onSubmit y validacion de fechas y titulo
   const onSubmit = (event) => {
     event.preventDefault();
+    setFormSubmitted(true)
     {/** Calcular diferencia en segundos entre las dos fechas */}
     const difference = differenceInSeconds(formValues.end, formValues.start)
     {/** Si la fecha NO es un numero o es menor a 0 retorna un error */}
@@ -119,7 +128,7 @@ export const CalendarModal = () => {
           <label>Titulo y notas</label>
           <input
             type="text"
-            className="form-control"
+            className={`form-control ${titleClass}`}
             placeholder="Título del evento"
             name="title"
             autoComplete="off"
@@ -145,7 +154,7 @@ export const CalendarModal = () => {
             Información adicional
           </small>
         </div>
-
+        {/* BOTON SUBMIT */}
         <button type="submit" className="btn btn-outline-primary btn-block">
           <i className="far fa-save"></i>
           <span> Guardar</span>
